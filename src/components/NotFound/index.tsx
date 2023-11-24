8d872766d1b7dbe36e496b277ef455ba5848d13a
urmatsagynbaev <megabmx4477@gmail.com>
2023-11-25 05:32:49 +0600

    last

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -13,7 +13,7 @@ export const NotFound = ({
   const navigate = useNavigate()
 
   return (
-    <div className="flex items-center justify-center w-full h-screen px-16 md:px-0">
+    <div className="flex items-center justify-center w-full h-[80vh] px-2 sm:px-16 md:px-0">
       <div className="flex flex-col items-center justify-center px-4 py-8 bg-white border border-gray-200 rounded-lg shadow-2xl md:px-8 lg:px-24">
         <p className="text-6xl font-bold tracking-wider text-gray-300 md:text-7xl lg:text-9xl">404</p>
         <p className="mt-4 text-2xl font-bold tracking-wider text-gray-500 md:text-3xl lg:text-5xl">{title || 'Страница не найдена'}</p>
